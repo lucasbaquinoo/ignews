384e3a5e272286adcea90c92ef4fc3ba56200749
lucasbaquinoo <lucasbaquino@icloud.com>
2021-07-09 17:49:33 -0300

    strapi webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
+import Stripe from 'stripe'
+import { stripe } from '../../services/stripe';
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -13,8 +15,26 @@ async function buffer(readable: Readable) {
   return Buffer.concat(chunks)
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const buf = await buffer(req)
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
 
-  res.status(200).json({ ok: true })
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const buf = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    }
+
+    res.status(200).json({ ok: true })
+  } catch (error) {
+    res.setHeader('Allow', 'POST')
+    res.status(405).send('Method not allowed')
+  }
 }
